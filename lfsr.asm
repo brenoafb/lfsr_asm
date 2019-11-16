a1fd188e8fb1952ff5f0cc52948f544a0919c8d8
brenoafb <breno.afb@gmail.com>
2019-11-16 19:03:44 -0300

    Return lfsb as random number.

--- lfsr.asm
+++ lfsr.asm
@@ -10,54 +10,33 @@
 	;; initialized data is put in the data segment here
 	;;
 
-	seed dd 0x00cafeba
-
 	segment	.bss
 	;;
 	;; uninitialized data is put in the bss segment
 	;;
-	lfsr	resd	1
-
 	global asm_generate
 	segment	.text
 asm_generate:
-	mov	ecx, 24			; counter
-	push	eax
-	mov	edx, [eax]		; register
-	xor	eax, eax		; clear out eax
-loop:
-	cmp	ecx, 0		; check if counter is zero
-	je	end		; done
-	shl	eax, 1			 ; shift to result to left by one
-
-	xor	edi, edi		; bit
-	mov	esi, edx		; copy register
+	mov	eax, [esp + 4]
+	mov	esi, eax		; copy register
 	shr	esi, 0
 	mov	edi, esi		 ; bit = lfsr >> 0
 
-	mov	esi, edx
+	mov	esi, eax
 	shr	esi, 1
 	xor	edi, esi		 ; bit = bit ^ (lfsr >> 1)
 
-	mov	esi, edx
+	mov	esi, eax
 	shr	esi, 2
 	xor	edi, esi		; bit = bit ^ (lfsr >> 2)
 
-	mov	esi, edx
+	mov	esi, eax
 	shr	esi, 7
 	xor	edi, esi		 ; bit = bit ^ (lfsr >> 7)
 	and	edi, 0x1                 ; keep only lsb
 
-	mov	esi, edx		 ; copy register to tmp
-	and	esi, 1			 ; isolate lsb
-	or	eax, esi		 ; add lsb to result
-
-	shr	edx, 1			 ; shift register right
+	shr	eax, 1			 ; shift register right
 	shl	edi, 23			 ; shift bit to msb
-	or	edx, edi		 ; set msb to bit
+	or	eax, edi		 ; set msb to bit
 
-	dec	ecx			 ; decrement counter
-	jmp	loop
-end:    pop	ecx
-	mov	[ecx], edx
 	ret
--- main.c
+++ main.c
@@ -1,45 +1,87 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <time.h>
 
 #define SEED 0x00cafeba
 #define MASK 0x00ffffff  // ensure 24-bit
+// #define N    (16*1024*1024)
+#define N    (1024)
 
-uint32_t generate(uint32_t *lfsr);
-uint32_t asm_generate(uint32_t *lfsr);
+uint32_t generate(uint32_t lfsr);
+uint32_t asm_generate(uint32_t lfsr);
+int test(uint32_t seed);
+int test_period(uint32_t seed);
 
 int main(void) {
-  uint32_t init = time(NULL) & MASK;
-  // lfsr = time(NULL) & MASK;
-  uint32_t lfsr = SEED;
+  /*
+  uint32_t *cgen = calloc(sizeof(uint32_t), N);
+  if (!cgen) {
+    printf("Error allocating memory (%d)\n", 1);
+    return 1;
+  }
 
-  uint32_t rc = generate(&lfsr);
-  printf("lfsr(c)=%09u (0x%08x)\n", lfsr, lfsr);
+  uint32_t *asmgen = calloc(sizeof(uint32_t), N);
+  if (!asmgen) {
+    printf("Error allocating memory (%d)\n", 2);
+    return 2;
+  }
+  */
+
+  int period = test(SEED);
+  printf("Period=%d\n", period);
+
+  /*
+  printf("Generating (C)\n");
+  for (int i = 0; i < N; i++) {
+    printf("%d\r", i+1);
+    cgen[i] = generate(&lfsr);
+  }
+
+  printf("\nGenerating (asm)\n");
   lfsr = SEED;
-  uint32_t ra = asm_generate(&lfsr);
-  printf("lfsr(asm)=%09u (0x%08x)\n", lfsr, lfsr);
-  printf("sd=%09u (0x%08x)\n", SEED, SEED);
-  printf("rc=%09u (0x%08x)\n", rc, rc);
-  printf("ra=%09u (0x%08x)\n", ra, ra);
+  for (int i = 0; i < N; i++) {
+    printf("%d\r", i);
+    asmgen[i] = asm_generate(&lfsr);
+  }
 
-  // for (int i = 0; i < 10; i++) {
-  //   uint32_t r = generate();
-  //   printf("%09u (0x%08x)\n", r, r);
-  // }
-
-  return 0;
+  free(cgen);
+  free(asmgen);
+  */
+    return 0;
 }
 
-uint32_t generate(uint32_t *lfsr) {
-  uint32_t bit;
-  uint32_t x = 0;
-  for (int i = 0; i < 24; i++) {
-    x <<= 1;
-    /* polynomial: x^24 + x^23 + x^22 + x17 + 1 */
-    bit = ((*lfsr >> 0) ^ (*lfsr >> 1) ^ (*lfsr >> 2) ^ (*lfsr >> 7)) & 0x1;
-    // printf("%d", bit & 0x1);
-    x |= *lfsr & 0x1;
-    *lfsr = (*lfsr >> 1) | (bit << 23);
-  }
-  return x;
+uint32_t generate(uint32_t lfsr) {
+  /* polynomial: x^24 + x^23 + x^22 + x17 + 1 */
+  uint32_t bit = ((lfsr >> 0) ^ (lfsr >> 1) ^ (lfsr >> 2) ^ (lfsr >> 7)) & 0x1;
+  return ((lfsr >> 1) | (bit << 23));
+}
+
+int test(uint32_t seed) {
+  uint32_t lfsr = seed & MASK;
+  int period = 0;
+  do {
+    uint32_t cgen = generate(lfsr);
+    uint32_t asmgen = asm_generate(lfsr);
+    if (cgen != asmgen) {
+      printf("\nError at i=%d; c=0x%x (%d), asm=0x%x (%d)\n", period, cgen, cgen, asmgen, asmgen);
+      return period;
+    } else {
+      period++;
+      lfsr = cgen;
+    }
+  } while (lfsr != (seed & MASK));
+
+  printf("\nPass\n");
+  return period;
+}
+
+int test_period(uint32_t seed) {
+  uint32_t lfsr = seed & MASK;
+  int period = 0;
+  do {
+    lfsr = generate(lfsr);
+    period++;
+  } while (lfsr != (seed & MASK));
+  return period;
 }
