7d57673c9d7bdc2afda7e84cdd86cc2fd53fcee7
brenoafb <breno.afb@gmail.com>
2019-11-20 20:50:49 -0300

    Computar número pseudoaleatório bit a bit.

--- lfsr.asm
+++ lfsr.asm
@@ -1,4 +1,4 @@
-	;; file: lfsr.asm
+;; file: lfsr.asm
 	;; To create executable:
 	;; nasm -f elf lfsr.asm
 	;; gcc -m32 -o lfsr lfsr.o driver.c asm_io.o
@@ -10,33 +10,55 @@
 	;; initialized data is put in the data segment here
 	;;
 
+	seed dd 0x00cafeba
+
 	segment	.bss
 	;;
 	;; uninitialized data is put in the bss segment
 	;;
+	lfsr	resd	1
+
 	global asm_generate
 	segment	.text
 asm_generate:
+	mov	ecx, 24			; counter
 	mov	eax, [esp + 4]
-	mov	esi, eax		; copy register
+	push	eax
+	mov	edx, [eax]		; register
+	xor	eax, eax		; clear out eax
+loop:
+	cmp	ecx, 0			; check if counter is zero
+	je	end			; done
+	shl	eax, 1			; shift to result to left by one
+
+	xor	edi, edi		; bit
+	mov	esi, edx		; copy register
 	shr	esi, 0
 	mov	edi, esi		 ; bit = lfsr >> 0
 
-	mov	esi, eax
+	mov	esi, edx
 	shr	esi, 1
 	xor	edi, esi		 ; bit = bit ^ (lfsr >> 1)
 
-	mov	esi, eax
+	mov	esi, edx
 	shr	esi, 3
 	xor	edi, esi		; bit = bit ^ (lfsr >> 2)
 
-	mov	esi, eax
+	mov	esi, edx
 	shr	esi, 4
 	xor	edi, esi		 ; bit = bit ^ (lfsr >> 7)
 	and	edi, 0x1                 ; keep only lsb
 
-	shr	eax, 1			 ; shift register right
-	shl	edi, 23			 ; shift bit to msb
-	or	eax, edi		 ; set msb to bit
+	mov	esi, edx		 ; copy register to tmp
+	and	esi, 1			 ; isolate lsb
+	or	eax, esi		 ; add lsb to result
 
+	shr	edx, 1			 ; shift register right
+	shl	edi, 23			 ; shift bit to msb
+	or	edx, edi		 ; set msb to bit
+
+	dec	ecx			 ; decrement counter
+	jmp	loop
+end:    pop	ecx
+	mov	[ecx], edx
 	ret
--- main.c
+++ main.c
@@ -9,15 +9,15 @@
 #define N    16
 #define M    (1024*1024)
 
-uint32_t generate(uint32_t lfsr);
-uint32_t asm_generate(uint32_t lfsr);
+uint32_t generate(uint32_t *lfsr);
+uint32_t asm_generate(uint32_t *lfsr);
 int test(uint32_t seed);
 int test_period(uint32_t seed);
 double chi_squared(uint32_t *frequencies);
 
 int main(void) {
 
-  int period = test(SEED);
+  int period = test_period(SEED);
   printf("Period=%d\n", period);
 
   uint32_t categories[N];
@@ -26,9 +26,8 @@ int main(void) {
   uint32_t lfsr = SEED & MASK;
   clock_t c_clock = clock();
   for (int i = 0; i < N*M; i++) {
-    uint32_t r = generate(lfsr);
+    uint32_t r = generate(&lfsr);
     categories[r/M]++;
-    lfsr = r;
   }
 
   c_clock = clock() - c_clock;
@@ -39,44 +38,50 @@ int main(void) {
   for (int i = 0; i < N; i++) {
     printf("[%d,%d]: %d\n", i, i+1, categories[i]);
   }
+  printf("Chi Squared = %lf\n", chi_squared(categories));
 
   memset(categories, 0, sizeof(uint32_t) * N);
-  printf("\nASM code\n");
+  printf("C code\n");
   lfsr = SEED & MASK;
-
   clock_t asm_clock = clock();
-
   for (int i = 0; i < N*M; i++) {
-    uint32_t r = asm_generate(lfsr);
+    uint32_t r = asm_generate(&lfsr);
     categories[r/M]++;
-    lfsr = r;
   }
 
   asm_clock = clock() - asm_clock;
   double asm_time = ((double) asm_clock) / CLOCKS_PER_SEC;
-  printf("ASM time: %f\n", asm_time);
+  printf("asm time: %f\n", asm_time);
 
   printf("Generation intervals:\n");
   for (int i = 0; i < N; i++) {
     printf("[%d,%d]: %d\n", i, i+1, categories[i]);
   }
-
   printf("Chi Squared = %lf\n", chi_squared(categories));
+
 
   return 0;
 }
 
-uint32_t generate(uint32_t lfsr) {
-  /* polynomial: x^24 + x^23 + x^21 + x^20 + 1 */
-  uint32_t bit = ((lfsr >> 0) ^ (lfsr >> 1) ^ (lfsr >> 3) ^ (lfsr >> 4)) & 0x1;
-  return ((lfsr >> 1) | (bit << 23));
+uint32_t generate(uint32_t *lfsr) {
+  uint32_t bit;
+  uint32_t x = 0;
+  for (int i = 0; i < 24; i++) {
+    x <<= 1;
+    /* polynomial: x^24 + x^23 + x^22 + x17 + 1 */
+    bit = ((*lfsr >> 0) ^ (*lfsr >> 1) ^ (*lfsr >> 3) ^ (*lfsr >> 4)) & 0x1;
+    x |= *lfsr & 0x1;
+    *lfsr = (*lfsr >> 1) | (bit << 23);
+  }
+  return x;
 }
 
+/*
 int test(uint32_t seed) {
   uint32_t lfsr = seed & MASK;
   int period = 0;
   do {
-    uint32_t cgen = generate(lfsr);
+    uint32_t cgen = generate(&lfsr);
     uint32_t asmgen = asm_generate(lfsr);
     if (cgen != asmgen) {
       printf("\nError at i=%d; c=0x%x (%d), asm=0x%x (%d)\n", period, cgen, cgen, asmgen, asmgen);
@@ -90,23 +95,24 @@ int test(uint32_t seed) {
   printf("\nPass\n");
   return period;
 }
+*/
 
 int test_period(uint32_t seed) {
   uint32_t lfsr = seed & MASK;
   int period = 0;
   do {
-    lfsr = generate(lfsr);
+    generate(&lfsr);
     period++;
   } while (lfsr != (seed & MASK));
   return period;
 }
 
 double chi_squared(uint32_t *frequencies) {
-	uint32_t accumulated_sum = 0;
+  uint32_t accumulated_sum = 0;
 
-	for (uint32_t i = 0;i < N;i++) {
-		accumulated_sum += ((frequencies[i] - M) * (frequencies[i] - M));
-	}
+  for (uint32_t i = 0;i < N;i++) {
+    accumulated_sum += ((frequencies[i] - M) * (frequencies[i] - M));
+  }
 
-	return accumulated_sum / M;
+  return accumulated_sum / M;
 }
